be2763f149e6716edff61090f339dddd2d7f7a20
makishev@yahoo.com <makishev@yahoo.com>
2019-12-26 21:43:59 +0600

    добавил задание

---
+++ Domain/MyFile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Domain
+{
+    public class MyFile
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Size { get; set; }
+    }
+}
---
+++ FilesClient/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using Domain;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FilesClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<MyFile> MyFiles { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyFiles = new List<MyFile>();
+            dataGrid.ItemsSource = MyFiles;
+        }
+
+
+        private void AddFileButton(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = null;
+            AddFile();       
+            dataGrid.ItemsSource = MyFiles;
+        }
+
+        private void DeleteFileButton(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DownloadFileButton(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveButton(object sender, RoutedEventArgs e)
+        {
+            var myFiles =  JsonConvert.SerializeObject(MyFiles);
+
+            using (var client = new TcpClient())
+            {
+                client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3231));
+                using (var stream = client.GetStream())
+                {
+                    var data = Encoding.UTF8.GetBytes(myFiles);
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+        }
+
+        public bool AddFile()
+        {
+            var newFile = new MyFile();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+
+                newFile.Name = openFileDialog.FileName;
+                newFile.Size = new FileInfo(openFileDialog.FileName).Length.ToString();
+                MyFiles.Add(newFile);
+                return true;
+            }
+            return false;
+        }
+
+        
+    }
+}
